0aff35d701ae0c4e5d2dc4e4c4c5394d557633b4
paarthmadan <madanhockey@hotmail.com>
2016-03-17 17:27:10 -0400

    cleaned up code
    converted vars to final, security

--- BluetoothAndroidArduino/app/src/main/java/com/example/paarthmadan/bluetoothandroidarduino/MainActivity.java
+++ BluetoothAndroidArduino/app/src/main/java/com/example/paarthmadan/bluetoothandroidarduino/MainActivity.java
@@ -6,6 +6,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,14 +18,11 @@ import java.util.Set;
 import java.util.UUID;
 import android.content.ActivityNotFoundException;
 import android.speech.RecognizerIntent;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Locale;
-
 import static android.content.pm.ActivityInfo.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
 
     BluetoothAdapter mBluetoothAdapter;
@@ -32,7 +30,9 @@ public class MainActivity extends Activity {
     BluetoothDevice mmDevice;
     OutputStream mmOutputStream;
     InputStream mmInputStream;
-    volatile boolean stopWorker;
+
+    final String btName = "HC-07";
+    final UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
 
 
@@ -83,6 +83,7 @@ public class MainActivity extends Activity {
 
     void findBT() {
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
         if (mBluetoothAdapter == null) {
 
         }
@@ -98,7 +99,7 @@ public class MainActivity extends Activity {
 
         if (pairedDevices.size() > 0) {
             for (BluetoothDevice device : pairedDevices) {
-                if (device.getName().equals("HC-07")) // this name have to be
+                if (device.getName().equals(btName)) // this name have to be
                 // replaced with your
                 // bluetooth device name
                 {
@@ -116,9 +117,7 @@ public class MainActivity extends Activity {
 
     void openBT() throws IOException {
 
-        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); // Standard
-        // SerialPortService
-        // ID
+
         mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
         mmSocket.connect();
         mmOutputStream = mmSocket.getOutputStream();
@@ -142,10 +141,6 @@ public class MainActivity extends Activity {
                 Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
             }
 
-
-
-
-
     }
 
     public void onActivityResult(int requestcode, int resultcode, Intent i){
@@ -158,7 +153,7 @@ public class MainActivity extends Activity {
 
                 if(resultcode == RESULT_OK && i != null){
 
-                    
+
                     ArrayList<String> results = i.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 
                     try{
@@ -177,7 +172,7 @@ public class MainActivity extends Activity {
     }
 
     void closeBT() throws IOException {
-        stopWorker = true;
+
         mmOutputStream.close();
         mmInputStream.close();
         mmSocket.close();
