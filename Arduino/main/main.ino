7e1134cac09e17ab0441646b8659a997594f20af
paarthmadan <madanhockey@hotmail.com>
2016-03-20 14:52:29 -0400

    added switch statement for letters

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -1,5 +1,5 @@
 int counter = 0; 
-String incomingByte;
+String incomingString;
 
 void setup() {
   Serial.begin(9600);
@@ -10,22 +10,114 @@ void setup() {
 void loop() {
 
 if (Serial.available() > 0) {
-                // read the incoming byte:
-                incomingByte = Serial.readString();
 
-                // say what you got:
+       
+                incomingString = Serial.readString();
+
+                Serial.println(incomingString);
+
+                int lengthOfString = incomingString.length() + 1;
+
+                char letters[lengthOfString]; 
                 
-                Serial.println(incomingByte);
-
-               
-               
-                char letters[incomingByte.length()+1]; 
-                incomingByte.toCharArray(letters, incomingByte.length() + 1);
+                incomingString.toCharArray(letters, lengthOfString);
 
                 for(int i = 0; i < sizeof(letters); i++){
-                  Serial.println(letters[i]);
+                  
+                  switch(letters[i]){
+
+                    case 'A':
+                      break;
+
+                    case 'B':
+                      break;                    
+
+                    case 'C':
+                      break;
+                    
+                    case 'D':
+                      break;
+                      
+                    case 'E':
+                      break;
+                      
+                    case 'F':
+                      break;
+                      
+                    case 'G':
+                      break;
+                      
+                    case 'H':
+                      break;
+                      
+                    case 'I':
+                      break;
+                      
+                    case 'J':
+                      break;
+                      
+                    case 'K':
+                      break;
+                      
+                    case 'L':
+                      break;
+                      
+                    case 'M':
+                      break;
+                      
+                    case 'N':
+                      break;
+                      
+                    case 'O':
+                      break;
+                      
+                    case 'P':
+                      break;
+                      
+                    case 'Q':
+                      break;
+                      
+                    case 'R':
+                      break;
+
+                    case 'S':
+                      break;
+
+                    case 'T':
+                      break;
+                    
+                    case 'U':
+                      break;
+
+                    case 'V':
+                      break;
+
+                    case 'W':
+                      break;
+
+                    case 'X':
+                      break;
+
+                    case 'Y':
+                      break;
+
+                    case 'Z':
+                      break;
+
+                    default:
+                      break;
+  
+                  }
+                
                 }
                 
         }
 
 }
+
+
+
+
+
+
+
